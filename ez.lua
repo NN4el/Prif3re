a6fe209644b41d8127a7041c237106d7de6c06c5
NN4el <115657595+NN4el@users.noreply.github.com>
2022-10-13 11:21:16 +0300

    thr

---
+++ ez.lua
@@ -0,0 +1,132 @@
+
+local SimpleSexGUI = Instance.new("ScreenGui")
+ 
+local FGUI = Instance.new("Frame")
+ 
+local btnNaked = Instance.new("TextButton")
+ 
+local btnSex = Instance.new("TextButton")
+ 
+local tbxVictim = Instance.new("TextBox")
+ 
+local lblFUCKEMALL = Instance.new("TextLabel")
+ 
+local ImageLabel = Instance.new("ImageLabel")
+ 
+local lbltitle = Instance.new("TextLabel")
+ 
+local TextLabel = Instance.new("TextLabel")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+SimpleSexGUI.Name = "симпл секс"
+SimpleSexGUI.Parent = game.CoreGui
+ 
+ 
+FGUI.Name = "FGUI"
+FGUI.Parent = SimpleSexGUI
+FGUI.BackgroundColor3 = Color3.new(255,255,255)
+FGUI.BorderSizePixel = 1
+FGUI.Position = UDim2.new(0,0, 0.667, 0)
+FGUI.Size = UDim2.new(0,317, 0,271)
+FGUI.Draggable = true
+ 
+ 
+ 
+lbltitle.Name = "Title"
+lbltitle.Parent = FGUI
+lbltitle.BackgroundColor3 = Color3.new(255,255,255)
+lbltitle.BorderSizePixel = 1
+lbltitle.Position = UDim2.new (0, 0,-0.122, 0)
+lbltitle.Size = UDim2.new (0, 317,0, 33)
+lbltitle.Visible = true
+lbltitle.Active = true
+lbltitle.Draggable = false
+lbltitle.Selectable = true
+lbltitle.Font = Enum.Font.SourceSansBold
+lbltitle.Text = "Трахац икслибирти"
+lbltitle.TextColor3 = Color3.new(0, 0, 0)
+lbltitle.TextSize = 30
+ 
+ 
+ 
+ 
+ 
+btnSex.Name = "секасе"
+btnSex.Parent = FGUI
+btnSex.BackgroundColor3 = Color3.new(255,255,255)
+btnSex.BorderSizePixel = 2
+btnSex.Position = UDim2.new (0.044, 0,0.229, 0)
+btnSex.Size = UDim2.new (0, 120,0, 31)
+btnSex.Visible = true
+btnSex.Active = true
+btnSex.Draggable = false
+btnSex.Selectable = true
+btnSex.Font = Enum.Font.SourceSansBold
+btnSex.Text = "Начать трахац"
+btnSex.TextColor3 = Color3.new(2, 0, 0)
+btnSex.TextSize = 20
+ 
+ 
+ 
+ 
+tbxVictim.Name = "VictimTEXT"
+tbxVictim.Parent = FGUI
+tbxVictim.BackgroundColor3 = Color3.new(255,255,255)
+tbxVictim.BorderSizePixel = 2
+tbxVictim.Position = UDim2.new (0.543, 0,0.229, 0)
+tbxVictim.Size = UDim2.new (0, 133,0, 27)
+tbxVictim.Visible = true
+tbxVictim.Active = true
+tbxVictim.Draggable = false
+tbxVictim.Selectable = true
+tbxVictim.Font = Enum.Font.SourceSansBold
+tbxVictim.Text = "НИК"
+tbxVictim.TextColor3 = Color3.new(0, 0, 0)
+tbxVictim.TextSize = 35
+ 
+lblFUCKEMALL.Name = "снят адежду с себя"
+lblFUCKEMALL.Parent = FGUI
+lblFUCKEMALL.BackgroundColor3 = Color3.new(255,255,255)
+lblFUCKEMALL.BorderSizePixel = 3
+lblFUCKEMALL.Position = UDim2.new (0.025, 0,0.856, 0)
+lblFUCKEMALL.Size = UDim2.new (0, 301,0, 27)
+lblFUCKEMALL.Visible = true
+lblFUCKEMALL.Font = Enum.Font.SourceSansBold
+lblFUCKEMALL.Text = "Я тебя бум бум бум"
+lblFUCKEMALL.TextColor3 = Color3.new(0, 0, 0)
+lblFUCKEMALL.TextSize = 30
+ 
+
+ 
+ 
+
+ 
+ 
+btnSex.MouseButton1Click:Connect(function()
+ 
+local player = tbxVictim.Text
+local stupid = Instance.new('Animation')
+stupid.AnimationId = 'rbxassetid://148840371'
+hummy = game:GetService("Players").LocalPlayer.Character.Humanoid
+pcall(function()
+    hummy.Parent.Pants:Destroy()
+end)
+pcall(function()
+    hummy.Parent.Shirt:Destroy()
+end)
+local notfunny = hummy:LoadAnimation(stupid)
+notfunny:Play()
+notfunny:AdjustSpeed(10)
+while hummy.Parent.Parent ~= nil do
+wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[tbxVictim.Text].Character.HumanoidRootPart.CFrame
+end
+end)
